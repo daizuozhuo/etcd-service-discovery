6b1006b47fb090c38302beb0f1ef5a646fd5a813
Dai Zuozhuo <daizuozhuo@gmail.com>
2015-09-30 20:58:09 +0800

    first commit

---
+++ master.go
@@ -0,0 +1,92 @@
+package discovery
+
+import (
+	"log"
+	"time"
+
+	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
+	"github.com/coreos/etcd/client"
+	"encoding/json"
+)
+
+type Master struct {
+	members  map[string]*Member
+	KeysAPI  client.KeysAPI
+}
+
+// Member is a client machine
+type Member struct {
+	InGroup bool
+	IP      string
+	Name    string
+	CPU    int
+}
+
+func NewMaster(endpoints []string) {
+	cfg := client.Config{
+		Endpoints:               endpoints,
+		Transport:               client.DefaultTransport,
+		HeaderTimeoutPerRequest: time.Second,
+	}
+
+	etcdClient, err := client.New(cfg)
+	if err != nil {
+		log.Fatal("Error: cannot connec to etcd:", err)
+	}
+
+	master := &Master{
+		members: make(map[string]*Member),
+		KeysAPI: client.NewKeysAPI(etcdClient),
+	}
+	go master.WatchWorkers()
+	return master
+}
+
+func (m *Master) AddWorker(info *WorkerInfo) {
+	member := &Member{
+		InGroup: true,
+		IP:      info.IP,
+		Name:    info.Name,
+		CPU:    info.CPU,
+	}
+	m.members[member.Name] = member
+}
+
+func (m *Master) UpdateWorker(info *WorkerInfo) {
+	member := m.members[info.Name]
+	member.InGroup = true
+}
+
+func (m *Master) WatchWorkers() {
+	api := m.KeysAPI
+	watcher := api.Watcher("workers/", &client.WatcherOptions{
+		Recursive: true,
+	})
+	for {
+		res, err := watcher.Next(context.Background())
+		if err != nil {
+			log.Println("Error watch workers:", err)
+			break
+		}
+		if res.Action == "expire" {
+			member, ok := m.members[res.Node.Key]
+			if ok {
+				member.InGroup = false
+			}
+		} else if res.Action == "set" || res.Action == "update"{
+			info := &WorkerInfo{}
+			err := json.Unmarshal([]byte(res.Node.Value), info)
+			if err != nil {
+				log.Print(err)
+			}
+			if _, ok := m.members[info.Name]; ok {
+				m.UpdateWorker(info)
+			} else {
+				m.AddWorker(info)
+			}
+		} else if res.Action == "delete" {
+			delete(m.members, res.Node.Key)
+		}
+	}
+
+}
---
+++ worker.go
@@ -0,0 +1,69 @@
+package discovery
+import (
+	"runtime"
+	"encoding/json"
+	"time"
+	"log"
+
+	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
+	"github.com/coreos/etcd/client"
+)
+
+type Worker struct {
+	Name 	string
+	IP 		string
+	KeysAPI  client.KeysAPI
+}
+
+// workerInfo is the service register information to etcd
+type WorkerInfo struct {
+	Name string
+	IP   string
+	CPU  int
+}
+
+func NewWorker(name, IP string, endpoints []string) *Worker {
+	cfg := client.Config{
+		Endpoints:               endpoints,
+		Transport:               client.DefaultTransport,
+		HeaderTimeoutPerRequest: time.Second,
+	}
+
+	etcdClient, err := client.New(cfg)
+	if err != nil {
+		log.Fatal("Error: cannot connec to etcd:", err)
+	}
+
+	w := &Worker{
+		Name: name,
+		IP: IP,
+		KeysAPI: client.NewKeysAPI(etcdClient)
+
+	}
+	go w.HeartBeat()
+	return w
+}
+
+
+func (w *Worker) HeartBeat() {
+	api := w.KeysAPI
+
+	for {
+		info := &WorkerInfo{
+			Name: w.Name,
+			IP:   w.IP,
+			CPU:  runtime.NumCPU(),
+		}
+
+		key := "workers/" + w.Name
+		value, _ := json.Marshal(info)
+
+		_, err := api.Set(context.Background(), key, string(value), &client.SetOptions{
+			TTL: time.Second * 10,
+		})
+		if err != nil {
+			log.Println("Error update workerInfo:", err)
+		}
+		time.Sleep(time.Second * 3)
+	}
+}
